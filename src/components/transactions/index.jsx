239b9d1771f6f9fcc75ff9afd6029bb3efe0c3d1
inspirasiprogrammer <bDa3qviHDaWS6aL>
2020-11-04 14:54:14 +0700

    Deposit

--- src/components/transactions/index.jsx
+++ src/components/transactions/index.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { useDispatch } from 'react-redux';
+import React, {useState, useEffect }  from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import { Navbar, Container, Form, Button, Row, Col } from "react-bootstrap";
+import { Navbar, Container, Form, Button, Row, Col, Dropdown } from "react-bootstrap";
 import logoImg from "../../assets/simplebank.png";
 import userImg from "../../assets/user.png";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../transactions/style.scss"
 import { logout } from '../../actions/userActions';
+import { deposit, withdrawal, transfer, saldo } from '../../actions/transactionActions';
 
 const Transactions = ({ history }) => {
   const dispatch = useDispatch();
@@ -16,6 +18,52 @@ const Transactions = ({ history }) => {
     history.push('/login');
   }
 
+  const userLogin = useSelector((state) => state.userLogin)
+  const { token } = userLogin
+
+  const [amountDeposit, setAmountDeposit] = useState("");
+  const [descDeposit, setDescDeposit] = useState("");
+  const [accountWithdrawal, setAccountWithdrawal] = useState("");
+  const [amountWithdrawal, setAmountWithdrawal] = useState("");
+  const [descWithdrawal, setDescWithdrawal] = useState("");
+  const [accountTransfer, setAccountTransfer] = useState("");
+  const [amountTransfer, setAmountTransfer] = useState("");
+  const [descTransfer, setDescTransfer] = useState("");
+
+  useEffect(() => {
+    setAmountDeposit("");
+    setDescDeposit("");
+    setAccountWithdrawal("");
+    setAmountWithdrawal("");
+    setDescWithdrawal("");
+    setAccountTransfer("");
+    setAmountTransfer("");
+    setDescTransfer("");
+  }, [])
+
+  useEffect(() => {
+    if (token) {
+      dispatch(saldo())
+    } 
+  }, [dispatch, history, token])
+
+  const transactionSaldo = useSelector((state) => state.transactionSaldo)
+  const { saldoTotal } = transactionSaldo
+  const accountDeposit = saldoTotal?.account?.account_number
+
+  const submitDepositHandler = (e) => {
+    e.preventDefault();
+    dispatch(deposit(accountDeposit, amountDeposit, descDeposit));
+  };
+  const submitWithdrawalHandler = (e) => {
+    e.preventDefault();
+    dispatch(withdrawal(accountWithdrawal, amountWithdrawal, descWithdrawal));
+  };
+  const submitTransferHandler = (e) => {
+    e.preventDefault();
+    dispatch(transfer(accountTransfer, amountTransfer, descTransfer));
+  };
+  
   return (
     <Container>
       <Tabs>
@@ -31,16 +79,22 @@ const Transactions = ({ history }) => {
               />
             </Navbar.Brand>
             <Navbar.Collapse className="justify-content-end">
-              <Navbar.Brand>
-                <img
-                  src={userImg}
-                  width="30"
-                  height="30"
-                  className="d-inline-block align-top"
-                  alt="user"
-                />
-                <Button variant="danger" className="btn-sm" style={{margin: '0 0 1px 20px'}} onClick={logoutHandler}>Logout</Button>
-              </Navbar.Brand>
+              <Dropdown id="dropdown-basic">
+                <Navbar.Brand>
+                  <Dropdown.Toggle className="customDropdown">
+                  <img
+                    src={userImg}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="user"
+                  />
+                  </Dropdown.Toggle>
+                </Navbar.Brand>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={logoutHandler}>Logout</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </Navbar.Collapse>
           </Navbar.Collapse>
         </Navbar>
@@ -54,24 +108,33 @@ const Transactions = ({ history }) => {
           <div className="d-flex justify-content-center my-4">
             <h1>DEPOSIT</h1>
           </div>
-          <Form className="mt-3">
-            <Form.Group as={Row} controlId="formPlaintextAccount">
+          <div className="mb-5">
+            <h4>Total Saldo : {saldoTotal != null && saldoTotal.account ? saldoTotal.account.saldo : 0} </h4>
+          </div>
+          <Form onSubmit={submitDepositHandler} className="mt-3">
+            {/* <Form.Group as={Row} controlId="formPlaintextAccount">
               <Form.Label column sm="2">
                 Account Number
               </Form.Label>
               <Col sm="10">
                 <Form.Control
                   type="account"
+                  value={accountDeposit}
+                  onChange={(e) => setAccountDeposit(e.target.value)}
                   placeholder="Input the destination account number"
                 />
               </Col>
-            </Form.Group>
+            </Form.Group> */}
             <Form.Group as={Row} controlId="formPlaintextAmount">
               <Form.Label column sm="2">
                 Total Amount
               </Form.Label>
               <Col sm="10">
-                <Form.Control type="amount" placeholder="Input the amount" />
+                <Form.Control 
+                  type="amount" 
+                  value={amountDeposit}
+                  onChange={(e) => setAmountDeposit(e.target.value)}
+                  placeholder="Input the amount" />
               </Col>
             </Form.Group>
             <Form.Group as={Row} controlId="exampleForm.ControlTextareaDesc">
@@ -79,7 +142,12 @@ const Transactions = ({ history }) => {
                 Description
               </Form.Label>
               <Col sm="10">
-                <Form.Control as="textarea" rows={3} />
+                <Form.Control 
+                  as="textarea" 
+                  rows={3} 
+                  value={descDeposit}
+                  onChange={(e) => setDescDeposit(e.target.value)}
+                />
               </Col>
             </Form.Group>
             <Col sm={{ span: 10, offset: 11 }}>
@@ -93,7 +161,10 @@ const Transactions = ({ history }) => {
           <div className="d-flex justify-content-center my-4">
             <h1>WITHDRAWAL</h1>
           </div>
-          <Form className="mt-3">
+          {/* <div className="mb-5">
+            <h4>Total Saldo : {saldoTotal.account.saldo} </h4>
+          </div> */}
+          <Form onSubmit={submitWithdrawalHandler} className="mt-3">
             <Form.Group as={Row} controlId="formPlaintextAccountWithdraw">
               <Form.Label column sm="2">
                 Account Number
@@ -101,6 +172,8 @@ const Transactions = ({ history }) => {
               <Col sm="10">
                 <Form.Control
                   type="account"
+                  value={accountWithdrawal}
+                  onChange={(e) => setAccountWithdrawal(e.target.value)}
                   placeholder="Input the destination account number"
                 />
               </Col>
@@ -110,7 +183,12 @@ const Transactions = ({ history }) => {
                 Total Amount
               </Form.Label>
               <Col sm="10">
-                <Form.Control type="amount" placeholder="Input the amount" />
+                <Form.Control 
+                  type="amount" 
+                  placeholder="Input the amount" 
+                  value={amountWithdrawal}
+                  onChange={(e) => setAmountWithdrawal(e.target.value)}
+                />
               </Col>
             </Form.Group>
             <Form.Group
@@ -121,7 +199,12 @@ const Transactions = ({ history }) => {
                 Description
               </Form.Label>
               <Col sm="10">
-                <Form.Control as="textarea" rows={3} />
+                <Form.Control 
+                  as="textarea" 
+                  rows={3} 
+                  value={descWithdrawal}
+                  onChange={(e) => setDescWithdrawal(e.target.value)}
+                />
               </Col>
             </Form.Group>
             <Col sm={{ span: 10, offset: 11 }}>
@@ -135,7 +218,10 @@ const Transactions = ({ history }) => {
           <div className="d-flex justify-content-center my-4">
             <h1>TRANSFER</h1>
           </div>
-          <Form className="mt-3">
+          {/* <div className="mb-5">
+            <h4>Total Saldo : {saldoTotal.account.saldo} </h4>
+          </div> */}
+          <Form onSubmit={submitTransferHandler} className="mt-3">
             <Form.Group as={Row} controlId="formPlaintextRecepient">
               <Form.Label column sm="2">
                 Recepient
@@ -143,6 +229,8 @@ const Transactions = ({ history }) => {
               <Col sm="10">
                 <Form.Control
                   type="account"
+                  value={accountTransfer}
+                  onChange={(e) => setAccountTransfer(e.target.value)}
                   placeholder="Input the Recepient "
                 />
               </Col>
@@ -152,7 +240,12 @@ const Transactions = ({ history }) => {
                 Total Amount
               </Form.Label>
               <Col sm="10">
-                <Form.Control type="amount" placeholder="Input the amount" />
+                <Form.Control 
+                  type="amount" 
+                  placeholder="Input the amount" 
+                  value={amountTransfer}
+                  onChange={(e) => setAmountTransfer(e.target.value)}
+                />
               </Col>
             </Form.Group>
             <Form.Group
@@ -163,7 +256,12 @@ const Transactions = ({ history }) => {
                 Description
               </Form.Label>
               <Col sm="10">
-                <Form.Control as="textarea" rows={3} />
+                <Form.Control 
+                  as="textarea" 
+                  rows={3} 
+                  value={descTransfer}
+                  onChange={(e) => setDescTransfer(e.target.value)}
+                  />
               </Col>
             </Form.Group>
             <Col sm={{ span: 10, offset: 11 }}>
@@ -173,9 +271,17 @@ const Transactions = ({ history }) => {
             </Col>
           </Form>
         </TabPanel>
+        <TabPanel>
+          {/* isi code mutasi rekening disini */}
+        </TabPanel>
       </Tabs>
     </Container>
   );
+  // return (
+  //   <div>
+
+  //   </div>
+  // )
 }
 
 export default Transactions;
